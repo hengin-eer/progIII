e625c7b7d4670351cd9d5b5715291af856509c9f
hengin-eer <hirokix0608@gmail.com>
2024-05-14 21:22:30 +0900

    :tada: new: `for in`構文の使用

---
+++ 6/js/for_in.js
@@ -0,0 +1,18 @@
+const fruits = [
+    {
+        key: "リンゴ",
+        price: 200,
+    },
+    {
+        key: "ミカン",
+        price: 140,
+    },
+    {
+        key: "ナシ",
+        price: 260,
+    },
+]
+
+for (let i in fruits) {
+    console.log(fruits[i]);
+}
